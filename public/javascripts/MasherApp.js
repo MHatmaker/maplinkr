991d798c62ee6b8806f8cb7e694272350a8a3cb7
MHatmaker <Michael.Hatmaker@gmail.com>
2014-06-15 10:30:38 -0700

    angular elements with undefined methods

--- public/javascripts/MasherApp.js
+++ public/javascripts/MasherApp.js
@@ -1,3 +1,5 @@
+'use strict';
+
 String.format = function() {
     // The string containing the format items (e.g. "{0}")
     // will and always has to be the first argument.
@@ -12,71 +14,100 @@ String.format = function() {
     }
     
     return theString;
-}
+};
 
-var MasherApp = angular.module("MasherApp", ['ngRoute', 'ui.bootstrap']);
-/* 
+var MasherApp = angular.module("MasherApp", ['ngRoute', 'ui.bootstrap'])
     .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    alert('MasherApp module route provider');
+    console.debug('MasherApp module route provider');
+    var isCollapsed = false;
     $routeProvider.
       when('/', {
-        // templateUrl: 'index',
-        templateUrl: '/partials/index',
+        templateUrl: 'index',
+        // templateUrl: '/partials/index',
         controller: MasherCtrl, reloadOnSearch: true
-      }).
-      when('/showDoc/:id', {
-        templateUrl: 'partials/readDoc',
-        controller: DocShowCtrl
       }).
       otherwise({
           redirectTo: '/'
       });
+    console.debug('html5Mode')
+    //alert('html5Mode before');
     $locationProvider.html5Mode(true);
+    console.debug('html5Mode again')
+    //alert('html5Mode after');
    }
-]); */
+]);
+
+   var clickMe = function(){
+        alert('clicked');
+        isCollapsed = !isCollapsed
+    }
+
 
 var MasherCtrl = function ($scope) {
-  alert('MasherCtrl - initialize some tabs');
+  console.debug('MasherCtrl - initialize collapsed bool');
+  // alert('MasherCtrl - initialize some tabs');
   $scope.isCollapsed = false;
+  console.log("init with isCollapsed = " + $scope.isCollapsed);
+};
+ 
+var TabsCtrl = function ($scope) {
+  console.debug('TabsCtrl - initialize some tabs');
+  // alert('MasherCtrl - initialize some tabs');
   var contentsText = 'This tab opens a typical web page which displays typical web page stuff, including a div with {0}  programmed with {1} embedded in it.  Right click on the link below and select open in a new window or open in a new tab.';
     
   $scope.tabs = [
     { title:'Google Maps', 
-      content: "a Google map", 
-      /* 
+      content:'a Google map', 
+ /*       
       //String.Format(contentsText, 'a Google map', 'google map content'),
       ref: "http://localhost:8080/googlemap",
       refLabel: "Web Site with Google Maps Embedded...",
       imgSrc: "stylesheets/images/googlemap.png",
       imgAlt: "Google Map",
        */
+      url: " '/views/partials/googlemaptab.tpl.jade' ",
       active: true,
       disabled: false
     },
     { title:'Leaflet/Cloudmade Maps', 
-      content: "a Leaflet/Cloudmade map",
-/* 
+      content:'a Leaflet/Cloudmade map',
+ /* 
       //String.Format(contentsText, 'a Leaflet/Cloudmade map', 'Leaflet content'),
       ref: "http://localhost:8080/leaflet}",
       refLabel: "Web Site with Leaflet/Cloudmade Maps Embedded...",
       imgSrc:  "stylesheets/images/Leaflet.png",
       imgAlt: "Leaflet/Cloudmade Maps",
        */
+      url: " '/views/partials/leafletmaptab.tpl.jade' ",
+       
       active: false,
       disabled: false
     },
     { title:'ArcGIS Web Maps', 
-      content: "an ArcGIS Web Map",
-      /*
+      content:'an ArcGIS Web Map',
+     /*  
       // String.Format(contentsText, 'an ArcGIS Web Map', 'ArcGIS Online content'),
       ref: "http://localhost:8080/arcgis",
       refLabel: "Web Site with ArcGIS Web Maps Embedded...",
       imgSrc: "stylesheets/images/arcgis.png",
       imgAlt: "ArcGIS Web Maps",
        */
+      url: " '/views/partials/arcgismaptab.tpl.jade' ",
+       
       active: false,
       disabled: false
     }
   ];
+   $scope.currentTab = $scope.tabs[0].url; //'googlemaptab.tpl.html';
+   console.log("currentTab - url initialized to " + $scope.currentTab);
 
+    $scope.onClickTab = function (tab) {
+        alert("clicked on " + tab.url);
+        $scope.currentTab = tab.url;
+        console.debug("clicked on " + tab.url);
+    }
+    
+    $scope.isActiveTab = function(tabUrl) {
+        return tabUrl == $scope.currentTab;
+    }
 };
