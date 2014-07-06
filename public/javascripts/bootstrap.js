7cfd35f1b235212f6d7405032197d09de2059169
MHatmaker <Michael.Hatmaker@gmail.com>
2014-07-06 15:07:44 -0700

    Added many width and height properties, better results, but not the best way to style

--- public/javascripts/bootstrap.js
+++ public/javascripts/bootstrap.js
@@ -15,27 +15,29 @@
             console.debug('bootstrap init method');
             
             var MasherApp = angular.module("app", ['ngRoute', 'ui.bootstrap', 'ui.layout'])
-                .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-                console.debug('MasherApp module route provider');
-                var isCollapsed = false;
-                 
-                $routeProvider.
-                  when('/', {
-                    templateUrl: 'partials/SystemSelector.jade',
-                    controller: MasherApp.MasherCtrl, reloadOnSearch: true
-                  }).
-                  when('/views/partials/:id',  {
-                    templateUrl: function(params){ return '/partials/' + params.id; },
-                    controller: MasherApp.MapCtrl, reloadOnSearch: true
-                  }).
-                  otherwise({
-                      redirectTo: '/'
-                  }); 
-                  
-                console.debug('html5Mode');
-                $locationProvider.html5Mode(true);
-                console.debug('html5Mode again')
-               }
+                .config(['$routeProvider', '$locationProvider', 
+                function($routeProvider, $locationProvider) {
+                    console.debug('MasherApp module route provider');
+                    var isCollapsed = false;
+                     
+                    $routeProvider.
+                      when('/', {
+                        templateUrl: 'partials/SystemSelector.jade',
+                        // templateUrl: '/',
+                        controller: MasherApp.MasherCtrl, reloadOnSearch: true
+                      }).
+                      when('/views/partials/:id',  {
+                        templateUrl: function(params){ return '/partials/' + params.id; },
+                        controller: MasherApp.MapCtrl, reloadOnSearch: true
+                      }).
+                      otherwise({
+                          redirectTo: '/'
+                      }); 
+                      
+                    console.debug('html5Mode');
+                    $locationProvider.html5Mode(true);
+                    console.debug('html5Mode again')
+                   }
             ]);
             
             AppController.start(MasherApp);
