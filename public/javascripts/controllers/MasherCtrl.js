2da67523d62db96a609811d6e91d4532f9eeb2df
MHatmaker <Michael.Hatmaker@gmail.com>
2014-09-16 17:46:35 -0700

    Handling $viewContentLoaded event now allows the collapser to be invoked, depending on the isFirstViewing member variable.
    Added overview to SystemSelector.jade

--- public/javascripts/controllers/MasherCtrl.js
+++ public/javascripts/controllers/MasherCtrl.js
@@ -1,27 +1,45 @@
 
 (function() {
     "use strict";
+    var isFirstViewing = true;
 
     console.log('MasherCtrl setup');
     define(['angular'], function(angular) {
         console.log('MasherCtrl define');
         var selfMethods = {};
         
-        function MasherCtrl($scope) {
+        function MasherCtrl($scope, $location, $route, $routeParams) {
             console.debug('MasherCtrl - initialize collapsed bool');
             // alert('MasherCtrl - initialize some tabs');
             $scope.isCollapsed = false;
             $scope.currentTab = null;
             console.log("init with isCollapsed = " + $scope.isCollapsed);
             
+            $scope.$on('$viewContentLoaded', function(){
+                // alert($route.current.templateUrl + ' is loaded !!');
+                // alert('templateUrl  is loaded !!');
+                if(isFirstViewing == false){
+                    $scope.summmaryCollapser();
+                }
+                else{
+                    isFirstViewing = false;
+                }
+            });
+            
             $scope.summmaryCollapser = function(){
                 $scope.$broadcast('CollapseSummaryEvent')
                 console.log("MasherCtrl isCollapsed before broadcast " + $scope.isCollapsed);
                 $scope.isCollapsed = !$scope.isCollapsed;
                 console.log("MasherCtrl isCollapsed after broadcast " + $scope.isCollapsed);
-            }
+            };
             selfMethods["summmaryCollapser"] = $scope.summmaryCollapser;
             console.debug(selfMethods);
+            
+            $scope.showMeTheMapClicked = function(){
+                console.log("currentTab - url reset to " + $scope.currentTab.url);
+                $location.path($scope.currentTab.url);
+                // $scope.summmaryCollapser();
+            };
         };
         
         MasherCtrl.prototype.sumClaps = function (){
@@ -30,11 +48,12 @@
         
         function init(App) {
             console.log('MasherCtrl init');
-            App.controller('MasherCtrl', ['$scope', MasherCtrl]);
+            App.controller('MasherCtrl', ['$scope', '$location', MasherCtrl]);
             return MasherCtrl;
         }
         function startArcGIS(){
             console.log("startArcGIS");
+            isFirstViewing = false;
             MasherCtrl.prototype.sumClaps();
         }
 
