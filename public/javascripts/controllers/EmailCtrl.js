336711dc0a8404646929032e04f278d26b429e1d
MHatmaker <Michael.Hatmaker@gmail.com>
2015-04-17 10:19:21 -0700

    Cleaned out some logging, and eliminating some resizing at fetch stage.
    Textarea is too wide to see scrollbar.

--- public/javascripts/controllers/EmailCtrl.js
+++ public/javascripts/controllers/EmailCtrl.js
@@ -8,10 +8,6 @@
         console.log('EmailCtrl define');
 
         function EmailCtrl($scope) {
-            // context.fullUrl = assembleUrl(); // AgoNewWindowConfig.gethref();
-            // var channel = AgoNewWindowConfig.masherChannel();
-            // context.fullUrl += "&channel=" + channel;
-            // $scope.urlText = ""; //context.fullUrl;
             var context = {
                 'fullUrl' : ''
             };
@@ -20,27 +16,9 @@
                 'urlText' : ''
             };
 
-            //resizeTextArea();
 
             function resizeTextArea() {
                 var textarea = document.getElementById('UrlCopyFieldID');
-                // var anElem = angular.element(textarea);
-                // anElem.scope().$apply();
-                // textarea.innerHTML = context.fullUrl;
-                // textarea.innerText = $scope.urlContext.fullUrl;
-                // textarea.value = $scope.urlContext.fullUrl;
-                // textarea.value = context.fullUrl;
-                /*
-                setTimeout(function(){
-                    // $scope.safeApply(function(){
-
-                    $scope.$apply(function(){
-                        $scope.urlContext.urlText = context.fullUrl;
-                        textarea.style.height = (textarea.scrollHeight) + 'px';
-                    });
-                }, 1000);
-                */
-                // $scope.urlContext.urlText = context.fullUrl;
                 textarea.style.height = (textarea.scrollHeight) + 'px';
             };
 
@@ -67,22 +45,12 @@
             }
 
             $scope.fetchUrl = function(){
-                context.fullUrl = assembleUrl(); // AgoNewWindowConfig.gethref();
-                $scope.urlContext.urlText = context.fullUrl;
-                console.log("in fetchUrl - check $scope.urlContext.urlText");
-                console.log($scope.urlContext.urlText);
-                /*
-                var contextScope = $scope;
-                $scope.safeApply(function(){
-                    //contextScope.urlText = context.fullUrl;
-                    resizeTextArea();
-                });
-                setTimeout(function(){
-                    //contextScope.urlText = context.fullUrl;
-                    resizeTextArea();
-                }, 1000);
-                */
-                resizeTextArea();
+                // context.fullUrl = assembleUrl(); // AgoNewWindowConfig.gethref();
+                // $scope.urlContext.urlText = context.fullUrl;
+                // console.log("in fetchUrl - check $scope.urlContext.urlText");
+                // console.log($scope.urlContext.urlText);
+                //
+                // resizeTextArea();
                 var docEl = document.getElementById("UrlCopyFieldID");
                 console.debug(docEl);
                 var urlEl = angular.element(docEl);
@@ -118,27 +86,10 @@
                 console.log($scope.urlContext.urlText);
                 console.log(contextScope.urlContext.urlText);
 
-                // var contextScope = $scope;
-                /*
-                $scope.safeApply(function(){
-                    // contextScope.urlText = context.fullUrl;
+                contextScope.safeApply(function(){
+                    console.log("SAFEAPPLY CALLBACK with text " + contextScope.urlContext.urlText );
                     resizeTextArea();
                 });
-                */
-                /*
-                contextScope.urlContext.urlText = context.fullUrl;
-                // $scope.$digest();
-                */
-                // setTimeout(function(){
-                    contextScope.safeApply(function(){
-                        // contextScope.urlContext.urlText = context.fullUrl.substr(0);
-                        console.log("SETTIMEOUT CALLBACK with text " + contextScope.urlContext.urlText );
-                        resizeTextArea();
-                    });
-                // }, 1000);
-
-
-                // resizeTextArea();
             });
         }
 
