97ea9b08c54a352c63449340665fa31219f18c27
MHatmaker <Michael.Hatmaker@gmail.com>
2015-05-08 18:12:09 -0700

    ng-enter working.
    Linted SearcherCtrlGrp.

--- public/javascripts/controllers/SearcherCtrlGrp.js
+++ public/javascripts/controllers/SearcherCtrlGrp.js
@@ -1,15 +1,16 @@
+/*global define */
 
-(function() {
+(function () {
     "use strict";
 
     console.log('SearcherCtrlGrp setup');
     define([
         'angular',
-        'lib/utils',
-    ], function(angular, utils) {
+        'lib/utils'
+    ], function (angular, utils) {
         console.log('SearcherCtrlGrp define');
-        var selfDict = {'portal': null}
-        var portalForSearch = null;
+        var selfDict = {'portal': null},
+            portalForSearch = null;
 
         function SearcherCtrlGrp($scope) {
             $scope.findGrpDisabled = false;
@@ -27,12 +28,12 @@
           /*
             var layoutPlugin = new ngGridLayoutPlugin();
 
-            $scope.updateLayout = function(){
+            $scope.updateLayout = function () {
               layoutPlugin.updateGridLayout();
             };
              */
             $scope.selectedItm = "Nada";
-            $scope.selectionChanged = function(rowItem,event){
+            $scope.selectionChanged = function (rowItem, event) {
                 console.debug(rowItem.entity);
                 console.debug(rowItem.entity.title   + '/' + rowItem.entity.thumbnail);
                 $scope.findMapsForGroup(rowItem.entity.id);
@@ -47,82 +48,87 @@
             $scope.gridGrpOptions = {
                 data: 'grpGriddata',
                 // enablePaging: true,
-                rowHeight: '50' ,
+                rowHeight: '50',
                 // plugins: [layoutPlugin],
                 multiSelect: false,
                 afterSelectionChange:  $scope.selectionChanged,
                 columnDefs: [
-                    {field:'thumbnail',
-                     width: '50px',
-                     displayName:'Img',
-                     cellTemplate: $scope.imgTmplt},
-                    {field:'title',
-                     width: '124px',
-                     displayName: 'Group'},
-                    {field: 'snippet',
-                     width: '124px',
-                     displayName: 'Description'},
-                    {field: 'id',
-                     visible: false,
-                     displayName: '#'}
+                    {
+                        field: 'thumbnail',
+                        width: '50px',
+                        displayName: 'Img',
+                        cellTemplate: $scope.imgTmplt
+                    },
+                    {
+                        field: 'title',
+                        width: '124px',
+                        displayName: 'Group'
+                    },
+                    {
+                        field: 'snippet',
+                        width: '124px',
+                        displayName: 'Description'
+                    },
+                    {
+                        field: 'id',
+                        visible: false,
+                        displayName: '#'
+                    }
                 ]
             };
 
-            var portal = null;
+            // portal = null;
                // find groups based on input keyword
-            $scope.findArcGISGroup = function() {
-              console.log('findArcGISGroup');
-              var keyword = $scope.searchTermGrp;
-              var params = {
-                q:  keyword,
-                num:20  //find 40 items - max is 100
-               };
-               portalForSearch.queryGroups(params).then(function (data) {
-                $scope.showGroupResults(data);
-               });
+            $scope.findArcGISGroup = function () {
+                console.log('findArcGISGroup');
+                var keyword = $scope.searchTermGrp,
+                    params = {
+                        q:  keyword,
+                        num: 20  //find 40 items - max is 100
+                    };
+                portalForSearch.queryGroups(params).then(function (data) {
+                    $scope.showGroupResults(data);
+                });
             };
 
-            $scope.calculateInstructionHeight = function(){
-                var label = angular.element(document.getElementById("grpSearchLabel"));
-                var instructions = document.getElementById("grpSrchInstId");
+            $scope.calculateInstructionHeight = function () {
+                var label = angular.element(document.getElementById("grpSearchLabel")),
+                    instructions = document.getElementById("grpSrchInstId"),
 
-                var instructionsHgt = instructions.offsetHeight;
-                // console.log("instructionsHgt " + instructionsHgt);
-                var srcTerm = angular.element(document.getElementById("groupFinder"));
-                var hgt = label[0].offsetHeight + instructionsHgt + srcTerm[0].offsetHeight;
+                    instructionsHgt = instructions.offsetHeight,
+                    // console.log("instructionsHgt " + instructionsHgt);
+                    srcTerm = angular.element(document.getElementById("groupFinder")),
+                    hgt = label[0].offsetHeight + instructionsHgt + srcTerm[0].offsetHeight;
                 // console.log("Instructions height : " + hgt);
                 return hgt;
-            }
+            };
 
-            $scope.calculateHeights = function(){
-                var vrbg = angular.element(document.getElementById("Verbage"));
-                var accHead = angular.element(document.getElementById("AccdianNews"));
-                var srchWrap = angular.element(document.getElementById("searchToolWrapperGroup"));
-                var marginborder = (1 + 1) * 2;
-                var accinnermarginborder = (1 + 9) * 2;
-                var instructionsHgt =  $scope.calculateInstructionHeight();
-                var acc0 = accHead[0];
-                var acc0Hgt = acc0.offsetHeight;
+            $scope.calculateHeights = function () {
+                var vrbg = angular.element(document.getElementById("Verbage")),
+                    accHead = angular.element(document.getElementById("AccdianNews")),
+                    marginborder = (1 + 1) * 2,
+                    accinnermarginborder = (1 + 9) * 2,
+                    instructionsHgt =  $scope.calculateInstructionHeight(),
                 // console.log("vrbg : " + vrbg[0].offsetHeight + " instructionsHgt " + instructionsHgt + " accHead " +  4 * (accHead[0].offsetHeight));
-                var gridTopHgt = 30 + 20; // ngTopPanel + ngViewPort
-                var availableHgt = vrbg[0].offsetHeight -  accinnermarginborder - gridTopHgt - instructionsHgt -
-                                    4 * (accHead[0].offsetHeight + marginborder);
+                    gridTopHgt = 30 + 20, // ngTopPanel + ngViewPort
+                    availableHgt = vrbg[0].offsetHeight -  accinnermarginborder - gridTopHgt - instructionsHgt -
+                                    4 * (accHead[0].offsetHeight + marginborder),
                 // console.log("availableHgt" + availableHgt);
-                var rowHeight = 50;
-                var headerHeight = 34;
-                var height = +($scope.grpGriddata.length * rowHeight + headerHeight);
+                    rowHeight = 50,
+                    headerHeight = 34,
+                    height = +($scope.grpGriddata.length * rowHeight + headerHeight);
                 if (height > availableHgt) {
                     height = availableHgt;
                 }
-                if (height < 120){
+                if (height < 120) {
                     height = 120;
                 }
                 return height;
             };
 
             $scope.getGridStyleGroup = function () {
-                var height = $scope.calculateHeights() - 8;
-                var heightStr = String(height) + "px";
+                var height = $scope.calculateHeights() - 8,
+                    heightStr = String(height) + "px";
                 // console.log("heightStr - getGridStyleGroup : " + heightStr);
                 return {
                     height: heightStr
@@ -130,8 +136,8 @@
             };
 
             $scope.getGridStyleWrapper = function () {
-                var height = $scope.calculateHeights();
-                var heightStr = String(height) + "px";
+                var height = $scope.calculateHeights(),
+                    heightStr = String(height) + "px";
                 // console.log("heightStr - getGridStyleWrapper : " + heightStr);
                 return {
                     height: heightStr
@@ -145,12 +151,11 @@
                 }, 250);
             };
 
-            $scope.showGroupResults = function(response) {
+            $scope.showGroupResults = function (response) {
                 //clear any existing results
                 console.log('$scope.showGroupResults');
 
                 if (response.total > 0) {
-                    var grddata = response.results;
                     //create the grid
                     $scope.grpGriddata = response.results;
                     // $scope.gridGrpOptions.data = response.results;
@@ -160,75 +165,76 @@
                         $scope.$apply();
                     }
                     $scope.redrawGrid();
-              } else {
-                dojo.byId('groupResults').innerHTML = '<h2>Group Results</h2><p>No groups were found. If the group is not public use the sign-in link to sign in and find private groups.</p>';
-              }
+                } else {
+                    document.getElementById('groupResults').innerHTML = '<h2>Group Results</h2><p>No groups were found. If the group is not public use the sign-in link to sign in and find private groups.</p>';
+                }
             };
 
-            $scope.findArcGISGroupMaps = function(portal, searchTermMap) {
-              utils.showLoading();
-              var keyword = searchTermMap; //dojo.byId('mapFinder').value;
-              var params = {
-                q: ' type:"Web Map" -type:"Web Mapping Application" ' + keyword,
-                num: 20
-              };
-              portal.queryItems(params).then(function (data) {
+            $scope.findArcGISGroupMaps = function (portal, searchTermMap) {
+                utils.showLoading();
+                var keyword = searchTermMap, //dojo.byId('mapFinder').value;
+                    params = {
+                        q: ' type:"Web Map" -type:"Web Mapping Application" ' + keyword,
+                        num: 20
+                    };
+                portal.queryItems(params).then(function (data) {
                     $scope.showMapResults(data);
                 });
             };
 
-            $scope.findMapsForGroup = function(gId)
-            {
-              utils.showLoading();
-              console.log("findMapsForGroup : " + gId);
-              var params = {
-                 q:  'id : ' +  gId,
-                 num:20  //find 40 items - max is 100
-                };
+            $scope.findMapsForGroup = function (gId) {
+                var group = null,
+                    params = {
+                        q: 'id : ' +  gId,
+                        num: 20  //find 40 items - max is 100
+                    };
 
-                portalForSearch.queryGroups(params).then(function(groups){
+
+                console.log("findMapsForGroup : " + gId);
+                utils.showLoading();
+
+                portalForSearch.queryGroups(params).then(function (groups) {
                 //get group title and thumbnail url
-                if (groups.results.length > 0) {
-                  console.log("group 0 results:");
-                  var group = groups.results[0];
-                  console.debug(group);
+                    if (groups.results.length > 0) {
+                        group = groups.results[0];
+                        //Retrieve the web maps and applications from the group and display
 
-                  //Retrieve the web maps and applications from the group and display
-                  var params = {
-                    q: ' type:"Web Map" -type:"Web Mapping Application"',
-                    num: 10
-                  };
-                  group.queryItems(params).then(function (data) {
-                        $scope.showMapResults(data);
-                    });
-                }
-              });
+                        params = {
+                            q: ' type:"Web Map" -type:"Web Mapping Application"',
+                            num: 10
+                        };
+                        console.log("group 0 results:");
+                        group.queryItems(params).then(function (data) {
+                            $scope.showMapResults(data);
+                        });
+                    }
+                });
             };
 
             // gets private groups as well
-            $scope.signInFromGroupTab = function() {
-              console.log("signInFromGroupTab");
-              self.portal = portalForSearch;
+            $scope.signInFromGroupTab = function () {
+                console.log("signInFromGroupTab");
+                self.portal = portalForSearch;
 
-              if ($scope.signInOutGrp.indexOf('In') !== -1) {
-                portalForSearch.signIn().then(function (loggedInUser) {
-                    $scope.$emit('SignInOutEmitEvent', true); //out
-                    $scope.findArcGISGroup(portalForSearch);   // update results
-                }, function (error) { //error so reset sign in link
-                    $scope.$emit('SignInOutEmitEvent', true); //in
-                });
-              } else {
-                portalForSearch.signOut().then(function (portalInfo) {
-                    $scope.$emit('SignInOutEmitEvent', false); //in
-                    findArcGISGroup(portalForSearch);
-                });
-              }
+                if ($scope.signInOutGrp.indexOf('In') !== -1) {
+                    portalForSearch.signIn().then(function (loggedInUser) {
+                        $scope.$emit('SignInOutEmitEvent', true); //out
+                        $scope.findArcGISGroup(portalForSearch);   // update results
+                    }, function (error) { //error so reset sign in link
+                        $scope.$emit('SignInOutEmitEvent', true); //in
+                    });
+                } else {
+                    portalForSearch.signOut().then(function (portalInfo) {
+                        $scope.$emit('SignInOutEmitEvent', false); //in
+                        $scope.findArcGISGroup(portalForSearch);
+                    });
+                }
             };
 
-            $scope.$on('SignInOutBroadcastEvent', function(event, isSignedIn) {
-                if(isSignedIn){
+            $scope.$on('SignInOutBroadcastEvent', function (event, isSignedIn) {
+                if (isSignedIn) {
                     $scope.signInOutGrp = "Sign Out";
-                }else{
+                } else {
                     $scope.signInOutGrp = "Sign In";
                 }
             });
@@ -236,15 +242,15 @@
 
             //display a list of groups that match the input user name
 
-            $scope.showMapResults = function(response) {
+            $scope.showMapResults = function (response) {
                 utils.hideLoading();
                 //clear any existing results
                 console.log("showMapResults");
                 $scope.$emit('OpenMapPaneEvent', { 'respData' : response });
             };
-            selfDict['setPortal'] = function(portal){
+            selfDict.setPortal = function (portal) {
                 portalForSearch = portal;
-            }
+            };
         }
 
         function init(App, portal) {
@@ -261,6 +267,6 @@
 
         return { start: init };
 
-  });
-
-}).call(this);
+    });
+}());
+// }).call(this);
