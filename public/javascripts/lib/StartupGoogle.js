dd25cb5f541d958b27b873cdcb7da43c608bd196
MHatmaker <Michael.Hatmaker@gmail.com>
2015-06-08 16:32:46 -0700

    Linted StartupGoogle, but not loadScript/skipScript part

--- public/javascripts/lib/StartupGoogle.js
+++ public/javascripts/lib/StartupGoogle.js
@@ -1,3 +1,10 @@
+/*global require*/
+/*global define*/
+/*global L*/
+/*global dojo*/
+/*global google*/
+
+
 // define('google', function () {
     // if (google) {
         // return google;
@@ -5,19 +12,19 @@
     // return {};
 // });
 
-var isGoogleLoaded = false;
-var isPlacesLoaded = false;
+var isGoogleLoaded = false,
+    isPlacesLoaded = false;
 
 function loadScript(scrpt, loadedTest, callback) {
     "use strict";
     var script = document.createElement('script');
     script.type = 'text/javascript';
     console.log('loadScript before append');
-    if(loadedTest == false){
+    if (loadedTest === false) {
         console.log("load google api library" + scrpt);
 
-        if(callback){
-            script.onload=callback;
+        if (callback) {
+            script.onload = callback;
         }
 
         // script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&'  + 'callback=skipScript';
@@ -25,23 +32,24 @@ function loadScript(scrpt, loadedTest, callback) {
         document.body.appendChild(script);
         script.src = scrpt + '&callback=' + callback;
         console.log('loadScript after append');
-    }
-    else{
+    } else {
         console.log("google api already loaded");
     }
 }
 
 function skipScript() {
+    "use strict";
     console.log('skipScript');
 }
 
 function initPlaces() {
+    "use strict";
     console.log('skipScript');
 }
 
 // window.onload = loadScript;
 
-(function() {
+(function () {
     "use strict";
     // require(['lib/MapHosterGoogle', 'lib/AgoNewWindowConfig']);
 
@@ -51,119 +59,120 @@ function initPlaces() {
         'controllers/StompSetupCtrl',
         'lib/AgoNewWindowConfig',
         'lib/utils'
-    ], function(MapHosterGoogle, StompSetupCtrl, AgoNewWindowConfig, utils) {
+    ], function (MapHosterGoogle, StompSetupCtrl, AgoNewWindowConfig, utils) {
         console.log('StartupGoogle define');
-        var CHANNEL = '/mapxtnt/';
-        var mph = null;
-        var gMap = null;
-        var loading;
-        var newSelectedWebMapId = "";
-        var pusherChannel = null;
-        var pusher = null;
+        var
+            gMap = null,
+            newSelectedWebMapId = "",
+            pusherChannel = null,
+            pusher = null;
 
         // loadScript();
-        console.debug(google);
+        // console.debug(google);
 
-        function getMap(){
+        function getMap() {
             return gMap;
         }
 
-        function resizeWebSiteVertical(isMapExpanded){
+        function resizeWebSiteVertical(isMapExpanded) {
             MapHosterGoogle.resizeWebSite(isMapExpanded);
         }
 
-        function resizeVerbageHorizontal(isMapExpanded){
+        function resizeVerbageHorizontal(isMapExpanded) {
             MapHosterGoogle.resizeVerbage(isMapExpanded);
         }
 
-        function resizeMapPane(isMapExpanded){
+        function resizeMapPane(isMapExpanded) {
 
             console.log("StartupGoogle.resizeMapPane : invalidateSize stub");
         }
 
-        var urlObject;
-        var configOptions;
-        var loading;
 
-        function configure(newMapId)
-        {
+        function configure(newMapId) {
+            var $inj,
+                evtSvc,
+                centerLatLng,
+                initZoom,
+                mapOptions = {},
+                mpcanhgt,
+                qlat,
+                qlon,
+                bnds,
+                zoomStr,
+                userName,
+                canelem = document.getElementById('map_canvas'),
+
+                openAgoWindow = function (channel, userName) {
+                    var url = "?id=" + newSelectedWebMapId + MapHosterGoogle.getGlobalsForUrl() + "&channel=" + channel + "&userName=" + userName;
+                    console.log("open new ArcGIS window with URI " + url);
+                    console.log("using channel " + channel + " with user name " + userName);
+                    AgoNewWindowConfig.setUrl(url);
+                    AgoNewWindowConfig.setChannel(channel);
+                    AgoNewWindowConfig.userName(userName);
+                    window.open(AgoNewWindowConfig.gethref() + "arcgis/" + url, newSelectedWebMapId, AgoNewWindowConfig.getSmallFormDimensions());
+                };
+
             newSelectedWebMapId = newMapId;
             console.log("newSelectedWebMapId " + newMapId);
-            window.loading = dojo.byId("loadingImg")
-            //This service is for development and testing purposes only. We recommend that you create your own geometry service for use within your applications.
-            // esri.config.defaults.geometryService = new esri.tasks.GeometryService("http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
 
-            //specify any default settings for your map
-            //for example a bing maps key or a default web map id
-            configOptions = {
-                webmap:newMapId,
-                title:"",
-                subtitle:"",
-                //arcgis.com sharing url is used modify this if yours is different
-                sharingurl:"http://arcgis.com/sharing/content/items",
-                //enter the bing maps key for your organization if you want to display bing maps
-                bingMapsKey:"/*Please enter your own Bing Map key*/"
-            }
+            window.loading = dojo.byId("loadingImg");
 
-            if( newSelectedWebMapId !== null)
-            {
-                if(AgoNewWindowConfig.isNameChannelAccepted() === false){
-                    var $inj = angular.injector(['app']);
-                    var evtSvc = $inj.get('StompEventHandlerService');
+            if (newSelectedWebMapId !== null) {
+                if (AgoNewWindowConfig.isNameChannelAccepted() === false) {
+                    $inj = angular.injector(['app']);
+                    evtSvc = $inj.get('StompEventHandlerService');
                     evtSvc.addEvent('client-MapXtntEvent', MapHosterGoogle.retrievedBounds);
                     evtSvc.addEvent('client-MapClickEvent',  MapHosterGoogle.retrievedClick);
 
                     StompSetupCtrl.setupPusherClient(evtSvc.getEventDct(),
-                        AgoNewWindowConfig.getUserName(), function(channel, userName){
-                        AgoNewWindowConfig.setUserName(userName);
-                        openAgoWindow(channel, userName);
+                        AgoNewWindowConfig.getUserName(), function (channel, userName) {
+                            AgoNewWindowConfig.setUserName(userName);
+                            openAgoWindow(channel, userName);
                         });
-                }
-                else{
+                } else {
+                    userName = AgoNewWindowConfig.getUserName();
                     openAgoWindow(AgoNewWindowConfig.masherChannel(false), userName);
                 }
-            }
-            else
-            {
-                var $inj = angular.injector(['app']);
-                var evtSvc = $inj.get('StompEventHandlerService');
+            } else {
+                $inj = angular.injector(['app']);
+                evtSvc = $inj.get('StompEventHandlerService');
                 evtSvc.addEvent('client-MapXtntEvent', MapHosterGoogle.retrievedBounds);
                 evtSvc.addEvent('client-MapClickEvent',  MapHosterGoogle.retrievedClick);
 
                 console.debug(AgoNewWindowConfig);
                 // var centerLatLng = new google.maps.LatLng(41.8, -87.7);
-                var centerLatLng = new google.maps.LatLng(41.880146, -87.623294);
-                var initZoom = 13;
+                centerLatLng = new google.maps.LatLng(41.880146, -87.623294);
+                initZoom = 13;
 
-                if(AgoNewWindowConfig.testUrlArgs()){
-                    var qlat = AgoNewWindowConfig.lat();
-                    var qlon = AgoNewWindowConfig.lon();
+                if (AgoNewWindowConfig.testUrlArgs()) {
+                    qlat = AgoNewWindowConfig.lat();
+                    qlon = AgoNewWindowConfig.lon();
                     centerLatLng = new google.maps.LatLng(qlat, qlon);
-                    var bnds = AgoNewWindowConfig.getBoundsFromUrl();
+                    bnds = AgoNewWindowConfig.getBoundsFromUrl();
                     console.log("getBoundsFromUrl..................");
                     console.debug(bnds);
-                    var zoomStr = AgoNewWindowConfig.zoom();
+                    zoomStr = AgoNewWindowConfig.zoom();
                     initZoom = parseInt(zoomStr, 10);
                 }
 
-                var mpcanhgt = utils.getElemHeight('map_canvas');
+                mpcanhgt = utils.getElemHeight('map_canvas');
                 console.log("mpcanhgt before new google.map is " + mpcanhgt);
 
-                var mapOptions = {
-                  center: centerLatLng, //new google.maps.LatLng(41.8, -87.7),
-                  // center: new google.maps.LatLng(51.50, -0.09),
-                  zoom: initZoom,
-                  mapTypeId: google.maps.MapTypeId.ROADMAP
+                mapOptions = {
+                    center: centerLatLng, //new google.maps.LatLng(41.8, -87.7),
+                    // center: new google.maps.LatLng(51.50, -0.09),
+                    zoom: initZoom,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
                 };
 
-                var canelem = document.getElementById('map_canvas');
+
                 // console.log("before first invalidate : " + canelem.clientHeight);
                 // invalidateMapWrapper();
-                canelem = document.getElementById('map_canvas');
+
                 console.log("before map create : " + canelem.clientHeight);
                 console.log("create a google map with option: " + mapOptions.mapTypeId);
                 gMap = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-                canelem = document.getElementById('map_canvas');
+
                 console.log("before second invalidate : " + canelem.clientHeight);
 
                 // invalidateMapWrapper();
@@ -171,70 +180,77 @@ function initPlaces() {
                 // console.log("after second invalidate : " + canelem.clientHeight);
 
                 // loadScript('https://maps.googleapis.com/maps/api/js?libraries=places', isPlacesLoaded);
-                mph = MapHosterGoogle.start();
+                MapHosterGoogle.start();
                 MapHosterGoogle.config(gMap, google, google.maps.places);
                 MapHosterGoogle.resizeWebSite(true);
 
                 pusherChannel = AgoNewWindowConfig.masherChannel(false);
                 console.debug(pusherChannel);
                 pusher = StompSetupCtrl.createPusherClient(
-                        {'client-MapXtntEvent' : MapHosterGoogle.retrievedBounds,
+                    {
+                        'client-MapXtntEvent' : MapHosterGoogle.retrievedBounds,
                         'client-MapClickEvent' : MapHosterGoogle.retrievedClick,
-                        'client-NewMapPosition' : MapHosterGoogle.retrievedNewPosition},
-                        pusherChannel,
-                        AgoNewWindowConfig.getUserName(),
-                        function(channel, userName){
-                                AgoNewWindowConfig.setUserName(userName);
-                                }
-                        );
+                        'client-NewMapPosition' : MapHosterGoogle.retrievedNewPosition
+                    },
+                    pusherChannel,
+                    AgoNewWindowConfig.getUserName(),
+                    function (channel, userName) {
+                        AgoNewWindowConfig.setUserName(userName);
+                    }
+                );
+                if (!pusher) {
+                    console.log("failed to create Pusher in StartupGoogle");
+                }
             }
         }
 
-        function openAGOWindow(channel, userName){
-            var url = "?id=" + newSelectedWebMapId + MapHosterGoogle.getGlobalsForUrl() + "&channel=" + channel + "&userName=" + userName;
-            console.log("open new ArcGIS window with URI " + url);
-            console.log("using channel " + channel + " with user name " + userName);
-            AgoNewWindowConfig.setUrl(url);
-            AgoNewWindowConfig.setChannel(channel);
-            AgoNewWindowConfig.userName(userName);
-            window.open(AgoNewWindowConfig.gethref() + "arcgis/" + url, newSelectedWebMapId, AgoNewWindowConfig.getSmallFormDimensions());
-        }
+/*
+        function invalidateMapWrapper() {
 
-        function invalidateMapWrapper(){
-
-            var element = 'map_wrapper';
+            var element = 'map_wrapper',
+                wrapHgt,
+                wrapWdth,
+                // cnvsHgt,
+                cnvsWdth;
             console.log("MapHosterGoogle map_wrapper : invalidateSize");
             // gMap.invalidateSize(true);
-            var wrapHgt = utils.getElementDimension(element, 'height') + 1;
+            wrapHgt = utils.getElementDimension(element, 'height') + 1;
             console.log('reset ' + element + ' height to ' + wrapHgt);
             utils.setElementDimension(element, 'height', wrapHgt);
 
-            var wrapWdth = utils.getElementDimension(element, 'width');
+            wrapWdth = utils.getElementDimension(element, 'width');
             console.log('reset ' + element + ' width to ' + wrapWdth);
             utils.setElementDimension(element, 'width', wrapWdth);
 
             element = 'map_canvas';
             // gMap.invalidateSize(true);
-            var cnvsHgt = '100'; // wrapHgt; //utils.getElementDimension(element, 'height') + 1;
+            // cnvsHgt = '100'; // wrapHgt; //utils.getElementDimension(element, 'height') + 1;
             console.log('reset ' + element + ' height to ' + wrapHgt + 'px'); //cnvsHgt + '%');
             utils.setElementDimension(element, 'height', wrapHgt); // cnvsHgt, '%');
 
-            var cnvsWdth = utils.getElementDimension(element, 'width');
+            cnvsWdth = utils.getElementDimension(element, 'width');
             console.log('reset ' + element + ' width to ' + cnvsWdth);
             utils.setElementDimension(element, 'width', cnvsWdth);
         }
+*/
 
         function StartupGoogle() {
-        };
+            console.log('StartupGoogle unused block');
+        }
+
         function init() {
             console.log('StartupGoogle init');
             return StartupGoogle;
         }
 
-        return { start: init, config : configure, getMap: getMap,
-                 resizeWebSite: resizeWebSiteVertical, resizeVerbage: resizeVerbageHorizontal,
-                 resizeMapPane: resizeMapPane};
+        return {
+            start: init,
+            config : configure,
+            getMap: getMap,
+            resizeWebSite: resizeWebSiteVertical,
+            resizeVerbage: resizeVerbageHorizontal,
+            resizeMapPane: resizeMapPane
+        };
 
     });
-
-}).call(this);
+}());
